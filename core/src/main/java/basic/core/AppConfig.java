9fade19ce935d9315ea1a4887e480d6e1edbd795
Ujoonnee <dnwns0933@naver.com>
2022-08-16 11:30:08 +0900

    ~ AppConfig.java 리팩토링

---
+++ core/src/main/java/basic/core/AppConfig.java
@@ -0,0 +1,31 @@
+package basic.core;
+
+import basic.core.discount.DiscountPolicy;
+import basic.core.discount.FixDiscountPolicy;
+import basic.core.discount.RateDiscountPolicy;
+import basic.core.member.MemberRepository;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+import basic.core.member.MemoryMemberRepository;
+import basic.core.order.OrderService;
+import basic.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
