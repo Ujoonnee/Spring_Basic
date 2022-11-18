685b15e22fded1f4599b66e6b53999506c86b636
Ujoonnee <dnwns0933@naver.com>
2022-11-18 18:33:27 +0900

    ~ Configuration bytecode 3:39

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -2,15 +2,16 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price; //stateful field
+//    private int price; //stateful field
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + "price = " + price);
-        this.price = price; //문제
-    }
-
-    public int getPrice() {
+//        this.price = price; //문제
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 
 }
