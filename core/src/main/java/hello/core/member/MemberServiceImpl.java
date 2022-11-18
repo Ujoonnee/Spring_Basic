3d754d564f2b8fa55fe65f4ffa213a0eeefb6460
Ujoonnee <dnwns0933@naver.com>
2022-11-18 18:02:03 +0900

    ~ SingletonTest

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
